399ca8981c6185e11e9e5f964c5683fdeeeb8e20
lsf <3164941709@qq.com>
2024-05-15 20:57:51 +0800

    message20:testing for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,6 +7,26 @@ from django.test import LiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10  # 定义等待的最长时间
 class NewVisitorTest(LiveServerTestCase):
+  def test_layout_and_styling(self):  
+    # 张三访问首页  
+    self.browser.get(self.live_server_url)  
+    self.browser.set_window_size(1024, 768)  
+    # 她看到输入框完美地居中显示  
+    inputbox = self.browser.find_element(By.ID, 'id_new_item')  
+    self.assertAlmostEqual(  
+        inputbox.location['x'] + inputbox.size['width'] / 2,  
+        512,  
+        delta=10  
+    )
+    # 他新建一个清单,看到输入框仍然完美的居中显示  
+    inputbox.send_keys('testing')  
+    inputbox.send_keys(Keys.ENTER)  
+    self.wait_for_row_in_list_table('1:testing')
+    inputbox = self.browser.find_element(By.ID, 'id_new_item')
+    self.assertAlmostEqual(
+      inputbox.location['x'] + inputbox.size['width'] / 2,
+      512,
+      delta=10)
   def setUp(self):
     self.browser=webdriver.Chrome()
 
